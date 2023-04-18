15a755999b0c3012e366b9a4a565318ad8b15899
Shaina Couch <shainacouch21@gmail.com>
2023-04-18 13:46:10 -0400

    moose can ask a question

--- Program.cs
+++ Program.cs
@@ -3,8 +3,11 @@
 Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
 Console.WriteLine();
-MooseSays("H I, I'm E N T H U S I A S T I C !");
-MooseSays("Seriously, enthusiastic");
+MooseSays("Hi!");
+
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
+
 
 void MooseSays(string message)
 {
@@ -37,3 +40,25 @@ void MooseSays(string message)
                        `^^` `^^^`
     ");
 }
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
